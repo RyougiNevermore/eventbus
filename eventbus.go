65af367d44ecd343bf2eed381e5548cb7c16a3bb
RyougiNevermore <ryougi.nevermore@hotmail.com>
2021-07-18 16:12:38 +0800

    add localEventHandleStore

--- eventbus.go
+++ eventbus.go
@@ -3,6 +3,11 @@ package eventbus
 import (
 	"github.com/aacfactory/errors"
 	"runtime"
+	"sync"
+)
+
+const (
+	defaultGroupName = "_ebs_"
 )
 
 func NewDeliveryOptions() DeliveryOptions {
@@ -90,4 +95,73 @@ func (e *defaultEvent) Body() []byte {
 
 func getDefaultEventChanCap() int {
 	return runtime.NumCPU() * 256
+}
+
+type eventHandleUnit struct {
+	handle EventHandler
+	tags   []string
+}
+
+func newLocalEventHandleStore() *localEventHandleStore {
+	return &localEventHandleStore{
+		lock:       sync.Mutex{},
+		handlerMap: make(map[string][]eventHandleUnit),
+	}
+}
+
+type localEventHandleStore struct {
+	lock       sync.Mutex
+	handlerMap map[string][]eventHandleUnit
+}
+
+func (s *localEventHandleStore) put(addr string, tags []string, handle EventHandler) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	handles, has := s.handlerMap[addr]
+	if !has {
+		handles = make([]eventHandleUnit, 0, 1)
+	}
+	if tags == nil {
+		tags = make([]string, 0, 1)
+	}
+	handles = append(handles, eventHandleUnit{
+		handle: handle,
+		tags:   tags,
+	})
+	s.handlerMap[addr] = handles
+}
+
+func (s *localEventHandleStore) contains(addr string, tags []string) (has bool) {
+	_, has = s.get(addr, tags)
+	return
+}
+
+func (s *localEventHandleStore) get(addr string, tags []string) (handle EventHandler, has bool) {
+	handles, has0 := s.handlerMap[addr]
+	if !has0 {
+		return
+	}
+
+	for _, stored := range handles {
+		if tags == nil || len(tags) == 0 {
+			handle = stored.handle
+			has = true
+			return
+		}
+		mapped := 0
+		for _, tag := range tags {
+			for _, st := range stored.tags {
+				if tag == st {
+					mapped++
+					break
+				}
+			}
+		}
+		if mapped == len(tags) {
+			handle = stored.handle
+			has = true
+			return
+		}
+	}
+	return
 }
